28b91df493edf06bc1c6cbc1410103877d49c80d
Alina Shevelev <alinashevelev@MacBook-Pro-Alina.local>
2022-11-27 23:04:51 +0200

    added solution

---
+++ defangedIP/script.js
@@ -0,0 +1,29 @@
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+function defangIPaddr (str) {
+    
+    var defangedIP = '';
+    str.split('').forEach (function (num){
+        (num == '.') ? defangedIP += '[.]' :
+       defangedIP += num;
+    })
+    return defangedIP;
+};
+
+var str = '255.100.50.0';
+document.write (defangIPaddr (str));
+
+console.log(defangIPaddr (str));
+
+
+
